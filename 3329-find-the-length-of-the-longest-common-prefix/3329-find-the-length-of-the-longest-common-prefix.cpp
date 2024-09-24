e6c4ece3fd9ea88ffb8f88ba026303a5b818c2f9
Ankit <57409550+Ankit-Kharb@users.noreply.github.com>
2024-09-24 23:55:58 +0530

    Time: 618 ms (16.31%), Space: 146.7 MB (77.97%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,16 +1,57 @@
 class Solution {
 public:
+    struct Trie
+    {
+        char Current;
+        vector<Trie *> childerns;
+        Trie(char x)
+        {
+            Current = x;
+        }
+
+        void Insert(string s)
+        {
+            for(auto child: this->childerns)
+            {
+                if(child->Current == s[0])
+                {
+                    if(s.size() == 1)
+                        return ;
+                    return child->Insert(s.substr(1));
+                }
+            }
+            Trie * newT = new Trie(s[0]);
+            this->childerns.push_back(newT);
+
+            if(s.size() == 1)
+                return ;
+            return newT->Insert(s.substr(1));
+        }
+
+        bool Find(string s)
+        {
+            for(auto child: this->childerns)
+            {
+                if(child->Current == s[0])
+                {
+                    if(s.size() == 1)
+                        return true;
+                    return child->Find(s.substr(1));
+                }
+            }
+            return false;
+        }
+    };
+
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         
         unordered_set<string> mySet;
+        Trie * myTrie = new Trie('.');
 
         for(auto ele : arr1)
         {
             string temp  = to_string(ele);
-            for(int i=0;i<temp.size();i++)
-            {
-                mySet.insert(temp.substr(0, i+1));
-            }
+            myTrie->Insert(temp);
         }
 
 
@@ -21,13 +62,12 @@ public:
             for(int i=0;i<temp.size();i++)
             {
                 string str = temp.substr(0, i+1);
-                if(mySet.find(str) != mySet.end())
+                if(myTrie->Find(str))
                 {
                     ans = max(ans, (int)str.size());
-                    
                 }
             }
         }
         return ans;    
     }
-};
+};  
