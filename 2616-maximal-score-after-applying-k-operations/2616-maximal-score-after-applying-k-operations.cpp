63fb781aeecfe53eea714d3ff167ed62f89b1d5a
Ankit <57409550+Ankit-Kharb@users.noreply.github.com>
2024-10-14 22:23:33 +0530

    Time: 225 ms (9.48%), Space: 86.1 MB (9.23%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long>> myPQ;
+
+        sort(nums.begin(), nums.end());
+        long long rem = nums.size()-k;
+
+        
+        for(long long i=nums.size()-1; i>=rem , i>=0 ;i--)
+        {
+            //cout<<"Hello"<<" "<<k<<" "<<i<<endl;
+            myPQ.push((long long)nums[i]);
+        }
+       
+        long long ans = 0;
+        while(k--)
+        {
+            long long val = myPQ.top();
+            long long valAd = (val+2)/3;
+            ans += val;
+            myPQ.pop();
+            myPQ.push(valAd);
+        }
+        return ans;
+    }
+};
