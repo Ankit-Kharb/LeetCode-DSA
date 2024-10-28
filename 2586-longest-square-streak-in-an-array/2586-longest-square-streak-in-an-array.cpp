cdc05638fbf49287201f4f345b54b240f48f21cc
Ankit <57409550+Ankit-Kharb@users.noreply.github.com>
2024-10-28 18:58:54 +0530

    Time: 202 ms (6.67%), Space: 131.1 MB (13.83%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -13,9 +13,16 @@ public:
         {
             int sequence = 1;
             long long temp = nums[i];
+
             while(1)
             {
                 long long square = temp*temp;
+                if(myMap[square]>1)
+                {
+                    sequence += myMap[square];
+                    break;
+                }
+
                 //cout<<nums[i]<<" "<<square<<" "<<"hello"<<endl;
                 if(myMap[square] == 0 || square >= 10e5)
                 {
@@ -24,11 +31,12 @@ public:
 
                 //myMap[temp] = 0;
                 sequence+=1;
-
                 temp = square;
             }
             if(sequence>=2)
             ans = max(ans, sequence);
+
+            myMap[nums[i]] = sequence;
         }
         return ans;
     }
