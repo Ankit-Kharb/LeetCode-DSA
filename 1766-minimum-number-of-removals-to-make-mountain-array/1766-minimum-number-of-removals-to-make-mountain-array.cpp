d5f85af1ef885c5a4d22b970477826e124279e22
Ankit <57409550+Ankit-Kharb@users.noreply.github.com>
2024-10-30 22:56:15 +0530

    Time: 291 ms (6.06%), Space: 14.9 MB (43.99%) - LeetHub

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> LIS(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        vector<int>ans(n, 0);
+        
+        for(int i=0;i<n-1;i++)
+        {
+            ans[i] = 1;
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums[j] < nums[i])
+                {
+                    ans[i] = max(ans[i], ans[j]+1);
+                }
+            }
+        }
+        return ans;
+    }
+    int minimumMountainRemovals(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        vector<int>preLIS = LIS(nums);
+        reverse(nums.begin(), nums.end());
+        vector<int>postLIS = LIS(nums);
+        reverse(postLIS.begin(), postLIS.end());
+        reverse(nums.begin(), nums.end());
+
+        int lisSize = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(preLIS[i]<=1 or postLIS[i]<=1)
+            continue;
+            
+            lisSize = max(lisSize, preLIS[i]+postLIS[i]-1);
+            cout<<nums[i]<<" "<<preLIS[i]<<" "<<postLIS[i]<<endl;
+        }
+        return n - lisSize;
+    }
+};
+/*
+[2,1,1,5,6,2,3,1]
+
+
+*/
