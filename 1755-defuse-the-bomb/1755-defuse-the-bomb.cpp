159787a0b1cc7926d2d24b19fa31f46cc6fa2473
Ankit <57409550+Ankit-Kharb@users.noreply.github.com>
2024-11-19 00:32:55 +0530

    Time: 11 ms (5.02%), Space: 10.4 MB (60.07%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        
+        int start = 0;
+        int cSum = 0;
+         int n = code.size();
+
+        vector<int>ans(n);
+
+        if(k == 0)
+        {
+            for(int i=0;i<code.size();i++)
+            {
+                ans[i] = 0;
+            }
+        }
+
+         if(k>0)
+        {
+
+            start = k;
+            for(int i=0;i<k;i++)
+            {
+                cSum += code[i];
+            }
+
+            for(int i=0;i<n;i++)
+            {
+                cSum += code[start];
+                cSum -= code[i];
+
+                ans[i] = cSum;
+
+                start++;
+                start = (start+n)%n;
+            }
+        }
+
+        if(k<0)
+        {
+            k = -k;
+            start = n-k-1;
+            for(int i=n-1;i>start;i--)
+            {
+                cSum += code[i];
+            }
+
+            for(int i=n-1;i>=0;i--)
+            {
+                //cout<<"Test1 "<<start<<" "<<i<<" "<<cSum<<" "<<code[i]<<endl;
+                cSum += code[start];
+                cSum -= code[i];
+
+                ans[i] = cSum;
+
+                start--;
+                start = (start+n)%n;
+                cout<<"Test "<<start<<" "<<i<<" "<<cSum<<endl;
+            }
+        }   
+
+       
+
+        return ans;   
+    }
+};
